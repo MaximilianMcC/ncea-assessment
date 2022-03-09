a030862122c0ab22feb5fbf9d3864ee76e747e31
Max <93854112+MaximilianMcC@users.noreply.github.com>
2022-03-09 22:14:15 +1300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading;
+
 public class Program {
 
 	public static void Main(string[] args)
@@ -41,7 +44,7 @@ public class Program {
 
             case 1:
                 // Make game
-                //! MakeGame();
+                MakeGame();
                 break;
 
             case 2:
@@ -49,15 +52,10 @@ public class Program {
                 About();
                 break;
 
-            case 3:
-                // About
-                About();
-				break;
-            
             case 4:
-                // Exit
+                // About
                 Environment.Exit(0);
-                break;
+				break;
         }
 	}
 
@@ -93,62 +91,130 @@ public class Program {
 
 	void PlayGame()
 	{
-        // Basic setup
-		Utils utils = new Utils();
 
-        // Get the JSON
+        // Basic setup
+        Utils utils = new Utils();
         Root json = utils.GetJson();
+        Console.Clear();
+        utils.Line();
+
+        // Ask the user to select a quiz
+        string[] quizNames = new string[json.Quiz.ToArray().Length];
+        for (int i = 0; i < quizNames.Length; i++) quizNames[i] = json.Quiz[i].Name;
+        int quizSelect = utils.ArrowMenu("Select a quiz", quizNames);
+        
         int score = 0;
 
-        // print all of the questions
-        foreach (Quesions question in json.Questions)
+        // Loop through all of the questions in the quiz
+        foreach (QuestionObject question in json.Quiz[quizSelect].Questions)
         {
-            // Reset everything
+            // Put the questions and answers in a arrow menu
             Console.Clear();
             utils.Line();
-            Console.WriteLine("\n\n");
 
-            // Show the question in the arrow menu
             int answer = utils.ArrowMenu(question.Question, question.Answers.ToArray());
+
+            // Check for if they gave the correct answer
             if (answer == question.Answer)
             {
-                // Correct answer
-                score++;
-
+                // Correct
                 Console.ForegroundColor = ConsoleColor.Green;
-                utils.CentreText("Correct! score: " + score);
-                Console.ResetColor();
+                utils.CentreText("\n\nCorrect!");
+                score++;
             }
             else
             {
-                // Wrong answer
+                // Incorrect
                 Console.ForegroundColor = ConsoleColor.Red;
-                utils.CentreText("Wrong! score: " + score);
-                Console.ResetColor();
+                utils.CentreText("\n\nIncorrect!");
             }
 
-            // Tell them that they can continue
-            utils.CentreText("\n\nPress any key to continue...");
+            // Reset the color, give them their total score then wait a few seconds
+            Console.ResetColor();
+            utils.CentreText("Total score: " + score);
+            utils.CentreText("\n\nPress any key to go onto the next question...");
             Console.ReadKey();
-
-            // Say that all of the questons have ended
-            Console.Clear();
-            utils.Line();
-            Console.WriteLine("\n\n");
-            utils.CentreText("You Have finished the quiz! Select and option below");
-
-            int returnAnswer = utils.ArrowMenu(items:new[] {"Back to menu", "Exit"});
-            if (returnAnswer == 0)
-            {
-                Menu();
-            }
-            else
-            {
-                utils.CentreText("\n\nThanks for playing. Your total score was: " + score);
-                utils.CentreText("Press any key to exit...");
-                Console.ReadKey();
-                Environment.Exit(0);
-            }
         }
+
+        // When they have finished a quiz then bring them back to the menu
+        Console.Clear();
+        utils.Line();
+        utils.CentreText($"You completed the quiz🤯🥳\nFinal score: {score}/{json.Quiz[quizSelect].Questions.ToArray().Length}");
+        utils.CentreText("\n\nPress any key to go back to the main menu...");
+        Console.ReadKey();
+        Menu();
+    }
+
+    // Let the user create a quiz
+    void MakeGame()
+    {
+        // Basic setup
+        Utils utils = new Utils();
+        Root json = utils.GetJson();
+
+
+        // Ask the user for a quiz name
+        Console.Write("What is the name of the quiz? ");
+        string quizName = Console.ReadLine().ToString().Trim();
+
+        // Ask them to enter a question, or answers
+        int choice = utils.ArrowMenu("Please select an option", new[] { "Add question", "Back to menu" });
+        if (choice == 0)
+        {
+            // Make initial variables
+            string questionName;
+            List<string> answers = new List<string>();
+            int answer = 0;
+
+            // Ask for the question and answers
+            Console.Write("question name: ");
+            questionName = Console.ReadLine().ToString().Trim();
+            
+            // Ask for how many answers
+            while (true)
+            {
+                Console.Write("How many answers do you want? ");
+                if (int.TryParse(Console.ReadLine(), out int numberOfAnswers))
+                {
+                    // Get all of the answers
+                    for (int i = 0; i < numberOfAnswers; i++)
+                    {
+                        Console.Write($"Answer for question {i++}: ");
+                        answers.Append(Console.ReadLine().Trim());
+                    }
+
+                    // get the correct answer
+                    Console.WriteLine("\n\nSelect the correct answer");
+                    for (int i = 0; i < answers.Count; i++)
+                    {
+                        Console.WriteLine($"{i++}) {answers[i]}");
+                    }
+                    while (true)
+                    {
+                        Console.Write("Correct answer: ");
+                        if (int.TryParse(Console.ReadLine(), out answer))
+                        {
+                            answer -= 1;
+                            Console.WriteLine("You have set the correct answer to " + answers[answer]);
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+
+            // Create the question and add it to the quiz
+            QuestionObject question = new QuestionObject()
+            {
+                Question = questionName,
+                Answers = answers,
+                Answer = answer
+            };
+
+
+
+            // Add it to the quiz
+        }
+
     }
 }
