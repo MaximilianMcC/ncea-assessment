53af9c81fe3f6d7654fb0726c343e37f6d543b1d
Max <93854112+MaximilianMcC@users.noreply.github.com>
2022-03-09 22:16:09 +1300

    Update JSON.cs

--- JSON.cs
+++ JSON.cs
@@ -1,10 +1,17 @@
 public class Root
 {
-    public List<Quesions> Questions { get; set; }
+    public List<Quiz> Quiz { get; set; }
+
 }
 
-public class Quesions
-{    
+public class Quiz
+{
+    public string Name { get; set; }
+    public List<QuestionObject> Questions { get; set; }
+}
+
+public class QuestionObject
+{
     public string Question { get; set; }
     public List<string> Answers { get; set; }
     public int Answer { get; set; }
